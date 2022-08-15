2d9e97ed21df91481b6f752a07961559ee80cf97
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-08-15 16:13:16 +0200

    Add get_filename_idx function.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "utils.h"
 
 int main(int argc, char* argv[]){
 	const int numb_thermal_zones = atoi(argv[1]);
-	if (numb_thermal_zones <= 0){
-		printf("'%s' is not correct arg.\n", argv[1]);
+	if (argc != 2){
+		puts("Incorrect number of args.");
 		return 1;
 	}
-
+	if (numb_thermal_zones <= 0){
+		printf("'%s' is not correct arg.\n", argv[1]);
+		return 2;
+	}
+	if (numb_thermal_zones > 100){
+		printf("%d args is too much.\n", numb_thermal_zones);
+		return 3;
+	}
+	char dupa[30] = "dupa";
+	printf("%s \n", get_filename_idx(dupa, 1));
 	return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,16 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char * get_filename_idx(char* gen_filename, int index){
+	char str_idx[5];
+	snprintf( str_idx, 5, "%d", index);
+	strcat(gen_filename, str_idx);
+	puts("dupa_blada");
+	return gen_filename;		
+}
 
 unsigned int read_thermal_zone(int zone_index){
-	
+	char path_thermal_zone[100] = "/sys/class/thermal/thermal_zone";	
 	return 10000;
 }
